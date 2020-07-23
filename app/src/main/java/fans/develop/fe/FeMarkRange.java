cf1404c86e9c3ffcbb3b84ae4085fe868bde24f9
wwx <593805475@qq.com>
2020-07-23 20:45:09 +0800

    临时

--- app/src/main/java/fans/develop/fe/FeMarkRange.java
+++ app/src/main/java/fans/develop/fe/FeMarkRange.java
@@ -4,10 +4,15 @@ package fans.develop.fe;
     移动范围计算和标记
  */
 public class FeMarkRange {
-    
+
+    //中心点
+    public int xGrid, yGrid;
+    //矩阵左上角
+    public int xGridStart, yGridStart;
+    //矩阵
     public FeTypeMark [][]range;
 
-    public FeMarkRange(FeInfoMap mapInfo){
+    public FeMarkRange(int xGrid, int yGrid, FeInfoMap mapInfo, int mov, FeTypeProfession type){
         ;
     }
 }
--- app/src/main/java/fans/develop/fe/FeTypeMark.java
+++ app/src/main/java/fans/develop/fe/FeTypeMark.java
@@ -5,7 +5,8 @@ package fans.develop.fe;
  */
 public enum FeTypeMark {
     NONE,
-    BLUE,
-    RED,
-    GREEN,
+    BLUE,   //移动范围
+    RED,    //攻击范围
+    GREEN,  //特效范围
+    ORANGE, //敌攻击范围
 }
