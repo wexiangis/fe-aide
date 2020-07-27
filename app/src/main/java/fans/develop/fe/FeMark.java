940b25e31eaa0444c5cc19ee507f6788256decb7
WWX <593805475@qq.com>
2020-07-27 17:28:58 +0800

    补全FeMark的攻击、特效范围计算

--- app/src/main/java/fans/develop/fe/FeMark.java
+++ app/src/main/java/fans/develop/fe/FeMark.java
@@ -1,10 +1,8 @@
 package fans.develop.fe;
 
-import fans.develop.fe.FeMark.Range;
-
-
 /*
     移动范围计算
+    调用构造函数即可得到 rangeMov(移动范围)、rangeHit(攻击范围)、rangeSpecial(特效范围)、
  */
 public class FeMark {
 
@@ -19,7 +17,7 @@ public class FeMark {
         xGrid, yGrid: 地图上的坐标
         typeProfession: 职业类型
         hit: 攻击距离
-        hitSpace: 近身空档(比如弓箭空档为1)
+        hitSpace: 近身空当(比如弓箭空档为1)
         special: 特效(杖、回复等)范围
      */
     public FeMark(int xGrid, int yGrid, FeInfoMap mapInfo, int mov, int typeProfession, int hit, int hitSpace, int special){
@@ -64,15 +62,15 @@ public class FeMark {
         count: 递归次数倒计时
         markValue: 标记值
      */
-    private void loopMark(int xGrid, int yGrid, Range range, int count, Boolean markValue){
+    private void loopMark(int xGrid, int yGrid, Boolean[][] range, int count, Boolean markValue){
         //递归计数用完
         if(count < 0)
             return;
         //该格已经标记过了
-        if(range.array[yGrid][xGrid] == markValue)
-            return;
+        // if(range[yGrid][xGrid] == markValue)
+        //     return;
         //标记当前格
-        range.array[yGrid][xGrid] = markValue;
+        range[yGrid][xGrid] = markValue;
         //计数减
         count -= 1;
         //递归
@@ -84,53 +82,85 @@ public class FeMark {
 
     /*
         根据移动范围计算攻击范围
+        hit： 攻击距离
+        hitSpace: 近身空当
      */
     private Range getRangeHit(Range rangeMov, int hit, int hitSpace){
         Range rangeHit = new Range(rangeMov, hit);
+        //哪有这种情况?
+        if(hitSpace >= hit || hit < 1)
+            return rangeHit;
         //在 rangeMov 矩阵定位的坐标要转到 rangeHit 坐标时加上该值
         int xErr = hit, yErr = hit;
+        //站在原地时的攻击范围
+        Boolean[][] rangeStayHit = new Boolean[hit * 2 + 1][hit * 2 + 1];
+        //画原地不动时的攻击范围
+        loopMark(hit, hit, rangeStayHit, hit, true);
+        //扣掉 hitSpace 范围
+        if(hitSpace > 0)
+            loopMark(hit, hit, rangeStayHit, hitSpace, false);
         //遍历 rangeMov 中所有可移动点
         for(int x = 0; x < rangeMov.width; x++){
             for(int y = 0; y < rangeMov.height; y++){
                 //是可移动点?
                 if(rangeMov.array[y][x]){
-                    //画攻击范围
-                    loopMark(x + xErr, y + yErr, rangeHit, hit, true);
-                    //扣掉hitSpace
-                    loopMark(x + xErr, y + yErr, rangeHit, hitSpace, false);
+                    //用 rangeStayHit 范围覆盖到当前可移动点上
+                    for(int xRH = x + xErr - hit, xRSH = 0; xRSH < rangeStayHit.length; xRH++, xRSH++){
+                        for(int yRH = y + yErr - hit, yRSH = 0; yRSH < rangeStayHit[0].length; yRH++, yRSH++){
+                            //是标记格？
+                            if(rangeStayHit[yRSH][xRSH])
+                                rangeHit.array[yRH][xRH] = true;
+                        }
+                    }
                 }
             }
         }
-        return null;
+        return rangeHit;
     }
 
     /*
-        根据移动范围计算攻击范围
+        根据移动范围计算特效范围
+        special: 特效距离
      */
     private Range getRangeSpecial(Range rangeMov, int special){
-        return null;
+        Range rangeSpecial = new Range(rangeMov, special);
+        if(special < 1)
+            return rangeSpecial;
+        //在 rangeMov 矩阵定位的坐标要转到 rangeSpecial 坐标时加上该值
+        int xErr = special, yErr = special;
+        //遍历 rangeMov 中所有可移动点
+        for(int x = 0; x < rangeMov.width; x++){
+            for(int y = 0; y < rangeMov.height; y++){
+                //是可移动点?
+                if(rangeMov.array[y][x]){
+                    //递归绘制 special 范围
+                    loopMark(x + xErr, y + yErr, rangeSpecial.array, special, true);
+                }
+            }
+        }
+        return rangeSpecial;
     }
 
     public class Range{
 
         //宽高
         public int width, height;
-        //矩阵左上角坐标
+        //矩阵左上角坐标(相对于地图)
         public int xGridStart, yGridStart;
         //矩阵中心坐标(相对于矩阵自身)
         public int xGridCenter, yGridCenter;
         //输出范围
         public Boolean[][] array;
-				
-				public int mapWidth, mapHeight;
+        //地图范围
+        public int mapWidth, mapHeight;
 
         /*
             xGrid, yGrid: 地图中的坐标
             mov: 移动力
          */
         public Range(int xGrid, int yGrid, int mov, int mapWidth, int mapHeight){
-						this.mapWidth = mapWidth;
-						this.mapHeight = mapHeight;
+            this.mapWidth = mapWidth;
+            this.mapHeight = mapHeight;
             //矩阵宽、高
             width = mov * 2 + 1;
             height = mov * 2 + 1;
@@ -148,8 +178,8 @@ public class FeMark {
             腰围扩增
          */
         public Range(Range rangeSrc, int addRad){
-						mapWidth = rangeSrc.mapWidth;
-						mapHeight = rangeSrc.mapHeight;
+            mapWidth = rangeSrc.mapWidth;
+            mapHeight = rangeSrc.mapHeight;
             //矩阵宽、高
             width = rangeSrc.width + addRad * 2;
             height = rangeSrc.height + addRad * 2;
